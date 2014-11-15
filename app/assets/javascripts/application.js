9c90889e1c5d14277ec5e60ac270810833747008
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2014-11-15 09:09:35 -0800

    Revert "Revert "Merge branch 'refs/heads/master' into ajaxified""
    This reverts commit 8eb975a9c1625417099d6054a072a3ceb8bad659.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,20 +1,30 @@
-angular.module('NoiseTwitter', []);
+angular.module('NoiseTwitter', ['ngRoute']);
 
-angular.module('NoiseTwitter').controller('MainController', function ($scope) {
-  $scope.tweets = [
-    {
-      content: 'You give love a bad name.'
-    },
-    {
-      content: 'Old McDonald had a farm.'
-    },
-    {
-      content: "I'm a little teapot, short and stout."
+angular.module('NoiseTwitter').config(function ($routeProvider) {
+  $routeProvider.when('/tweets/:tweet_id', {
+    controller: 'TweetController',
+    templateUrl: 'tweet.html',
+    resolve: {
+      tweet: function ($http, $route) {
+        return $http.get('/tweets/' + $route.current.params.tweet_id + '.json');
+      }
     }
-  ];
+  }).otherwise({
+    controller: 'MainController',
+    templateUrl: 'main.html',
+    resolve: {
+      tweets: function ($http) {
+        return $http.get('/tweets.json')
+      }
+    }
+  })
+}).controller('MainController', function ($scope, $http, tweets) {
+  $scope.tweets = tweets.data;
 
   $scope.addTweet = function () {
-    $scope.tweets.unshift({content: $scope.tweetContent});
+    $http.post('/tweets.json', {tweet: {content: $scope.tweetContent}}).success(function (data) {
+      $scope.tweets.unshift(data);
+    })
   };
 
   $scope.favorite = function (tweet) {
@@ -33,4 +43,6 @@ angular.module('NoiseTwitter').controller('MainController', function ($scope) {
   $scope.tweetContentInvalid = function () {
     return $scope.tweetContent.length === 0 || $scope.remainingCharacters() < 0;
   }
-});
+}).controller('TweetController', function ($scope, tweet) {
+  $scope.tweet = tweet.data;
+})
