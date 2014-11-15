d45c3615dbbca52c00919e1c9fd1d4b44a7292e7
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2014-11-14 18:07:24 -0800

    Starting router work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,24 @@
-angular.module('NoiseTwitter', []);
+angular.module('NoiseTwitter', ['ngRoute']);
 
-angular.module('NoiseTwitter').controller('MainController', function ($scope) {
+angular.module('NoiseTwitter').config(function ($routeProvider) {
+  $routeProvider.when('tweets/:tweet_id', {
+    controller: 'TweetController',
+    template: 'tweet.html',
+    resolve: {
+      tweet: function ($http, $route) {
+        return $http.get('/tweets/' + $route.current.params.tweet_id);
+      }
+    }
+  }).otherwise({
+    controller: 'MainController',
+    template: 'main.html',
+    resolve: {
+      tweets: function ($http) {
+        return $http.get('/tweets.json')
+      }
+    }
+  })
+}).controller('MainController', function ($scope) {
   $scope.tweets = [
     {
       content: 'You give love a bad name.'
