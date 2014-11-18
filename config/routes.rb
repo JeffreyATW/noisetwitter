83f18fa2fcab36f0967da5efb0690bbd78086f0e
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2014-11-18 09:23:03 -0800

    Putting tweets API into api subdirectory

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#index'
 
-  resources :tweets
+  resources :tweets, path: '/api/tweets'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
